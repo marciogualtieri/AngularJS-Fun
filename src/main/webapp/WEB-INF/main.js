eda4cbafb35b1d0e2c08a95200a23267335fedfe
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2016-02-22 12:50:54 +0000

    Refactored to use Angular custom directives

--- src/main/webapp/WEB-INF/main.js
+++ src/main/webapp/WEB-INF/main.js
@@ -1,37 +1,96 @@
-angular.module('marciogualtieri', [])
-  .controller('mainController', MainController);
+﻿﻿angular.module('myWebsite', ['ngRoute'])
+  .controller('mainController', MainController)
+    .controller('introductionController', IntroductionController)
+    .controller('historyController', HistoryController)
+    .controller('trainingController', TrainingController)
+    .factory('currentSpot', currentSpot)
+    .directive('myWebsiteActiveMenu', myWebsiteActiveMenu)
+  .config(function ($routeProvider) {
+    $routeProvider.when('/history', {
+      templateUrl: 'views/history.html',
+      controller: 'historyController'
+    });
+    $routeProvider.when('/training', {
+      templateUrl: 'views/training.html',
+      controller: 'trainingController'
+    });
+    $routeProvider.otherwise({
+      templateUrl: 'views/introduction.html',
+      controller: 'introductionController'
+    });
+  });
 
-/**
- * Main Page's AngularJS controller
- *
- * @param $scope: AngularJS scope
- */
-function MainController($scope) {
-  $scope.activeMenu = '';
-  $scope.isActive = isActive;
-  $scope.showMain = showMain;
-  $scope.showHistory = showHistory;
-  $scope.showCourses = showCourses;
-  $scope.showMain();
+function currentSpot() {
+  var activeMenuId = '';
+  var titleText = '';
 
-  function setView(view, menuId) {
-    $scope.view = view;
-    $scope.activeMenu = menuId;
+  return {
+    setCurrentSpot: function (menuId, title) {
+      activeMenuId = menuId;
+      titleText = title;
+    },
+    getActiveMenu: function () {
+      return activeMenuId;
+    },
+    getTitle: function () {
+      return titleText;
+    }
   }
+}
+
+function MainController($scope, currentSpot) {
+  $scope.isActive = isActive;
+  $scope.getTitle = getTitle;
 
   function isActive(menuId) {
-    return $scope.activeMenu == menuId;
+    return currentSpot.getActiveMenu() == menuId;
   }
 
-  function showMain() {
-    setView('main', '');
+  function getTitle() {
+    return currentSpot.getTitle();
+  }
+}
+
+function myWebsiteActiveMenu(currentSpot) {
+  return function (scope, element, attrs) {
+    var activeMenuId = attrs["myWebsiteActiveMenu"];
+    var activeTitle = attrs["myWebsiteActiveTitle"];
+    currentSpot.setCurrentSpot(activeMenuId, activeTitle);
+  }
+}
+
+function myWebsiteMenuId(currentSpot) {
+  var menuElements = [];
+
+  function setActive(element, menuId) {
+    if (currentSpot.getActiveMenu() == menuId) {
+      element.addClass('active');
+    } else {
+      element.removeClass('active');
+    }
   }
 
-  function showHistory() {
-    setView('history', 'History');
-  }
+  return function (scope, element, attrs) {
+    var menuId = attrs["myWebsiteMenuId"];
+    menuElements.push({ id: menuId, node: element });
 
-  function showCourses() {
-    setView('courses', 'Courses');
+    var watcherFn = function (watchScope) {
+      return watchScope.$eval('getActiveMenu()');
+    }
+    scope.$watch(watcherFn, function (newValue, oldValue) {
+      for (var i = 0; i < menuElements.length; i++) {
+        var menuElement = menuElements[i];
+        setActive(menuElement.node, menuElement.id);
+      }
+    });
   }
+}
+
+function IntroductionController(currentSpot) {
+}
+
+function HistoryController(currentSpot) {
+}
+
+function TrainingController(currentSpot) {
 }
